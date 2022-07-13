8a8fd81f1e262a0bfe98b0e0829a58173d374749
katyayakuts <katyayakut@gmail.com>
2022-07-13 15:01:20 +0400

    new check

--- check_string.sh
+++ check_string.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 str=$@
+str=$(echo $str | sed 's/ //g')
+
 l_str=${#str}
 
-str_num=$(echo $str | sed 's/[0-9]//g')
-str_lett=$(echo $str | sed 's/[aA-zZ]//g')
 str_sym=$(echo $str | tr -d '!@#$%^&()_+')
+
+str_num=$(echo $str | sed 's/[0-9]//g')
+
+str_lett=$(echo $str | sed 's/[aA-zZ]//g')
+
 
 s_str=${#str_sym}
 n_str=${#str_num}
